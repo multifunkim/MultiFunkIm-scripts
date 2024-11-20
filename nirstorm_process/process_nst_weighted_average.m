a8791dc4636f7fbcc62335a89a97be1876ecdf52
Edouard <edouard.delaire@icloud.com>
2024-11-20 13:30:13 -0500

    Add process

--- nirstorm_process/process_nst_weighted_average.m
+++ nirstorm_process/process_nst_weighted_average.m
@@ -1,7 +1,6 @@
-function varargout = process_nst_export_sensitivy_region( varargin )
-% Export the sensitivity of the montage to each region of a definied atlas.
-% For each channel, report the average sensitivity to eah region of the
-% atlas and export it as tsv file. 
+function varargout = process_nst_weighted_average( varargin )
+% Comput the weight average of fNIRS channels based on their sensitivity to
+% specific regions and export it as tsv file. 
 % Authors: Edouard Delaire (2024) 
 
 eval(macro_method);
@@ -9,14 +8,14 @@ end
 
 function sProcess = GetDescription() %#ok<DEFNU>
     % Description the process
-    sProcess.Comment     = 'Export nirs sensitivity to tsv';
+    sProcess.Comment     = 'Weighted averaged based on sensitivity';
     sProcess.FileTag     = '';
     sProcess.Category    = 'File';
     sProcess.SubGroup    = {'Multifunkim', 'fNIRS'};
-    sProcess.Index       = 7001;
+    sProcess.Index       = 7002;
     sProcess.Description = '';
     % Definition of the input accepted by this process
-    sProcess.InputTypes  = {'data', 'raw'};
+    sProcess.InputTypes  = {'data'};
     % Definition of the outputs of this process
     sProcess.OutputTypes = {'results'};
     sProcess.nInputs     = 1;
@@ -25,10 +24,22 @@ function sProcess = GetDescription() %#ok<DEFNU>
 
 
     % === Process description
-    % === CLUSTERS
-    sProcess.options.scouts.Comment = '';
-    sProcess.options.scouts.Type    = 'scout';
-    sProcess.options.scouts.Value   = {};
+
+    SelectOptions = {...
+        '', ...                            % Filename
+        '', ...                            % FileFormat
+        'open', ...                        % Dialog type: {open,save}
+        'Select weight...', ...     % Window title
+        'ImportData', ...                  % LastUsedDir: {ImportData,ImportChannel,ImportAnat,ExportChannel,ExportData,ExportAnat,ExportProtocol,ExportImage,ExportScript}
+        'single', ...                      % Selection mode: {single,multiple}
+        'files', ...                        % Selection mode: {files,dirs,files_and_dirs}
+        {{'.tsv'}, '*.tsv'}, ... % Available file formats
+        'ResultsIn'};                         % DefaultFormats: {ChannelIn,DataIn,DipolesIn,EventsIn,AnatIn,MriIn,NoiseCovIn,ResultsIn,SspIn,SurfaceIn,TimefreqIn}
+    
+    % Option definition
+    sProcess.options.weight_file.Comment = 'Sensitivity file:';
+    sProcess.options.weight_file.Type    = 'filename';
+    sProcess.options.weight_file.Value   = SelectOptions;
 
 
     SelectOptions = {...
@@ -63,50 +74,10 @@ function OutputFiles = Run(sProcess, sInputs) %#ok<DEFNU>
     % Save the new head model
     sStudy = bst_get('Study', sInputs.iStudy);
     
-    sForward    = in_bst_headmodel(sStudy.HeadModel(sStudy.iHeadModel).FileName);
-    sCortex     = in_tess_bst(sForward.SurfaceFile);
     
-    nChannel    = size(sForward.Gain,1);
     
-    % ROI selection
-    ROI     = sProcess.options.scouts.Value;
-    iAtlas  = find(strcmp( {sCortex.Atlas.Name},ROI{1}));
-    iRois   = cellfun(@(x)find(strcmp( {sCortex.Atlas(iAtlas).Scouts.Label},x)),   ROI{2});
-    
-    % Threshold : remove all sensitivity lower than -2db
-    threshold_value = -2; % in db
-
-    varTypes = ["string",  repmat("double", 1 , length(iRois))];
-    varNames = [{'Channel'}, {sCortex.Atlas(iAtlas).Scouts.Label}];
-    sz = [nChannel, length(varNames)];
-    
-    T = table('Size',sz,'VariableTypes',varTypes,'VariableNames',varNames);
-
-    % Average the gain of the two wavelength 
-    gain_matrix = squeeze(mean(sForward.Gain, 2));
-    max_gain    = max(max(gain_matrix));
-
-    iRow = 1;
-    for iPair = 1:nChannel
-
-        gain_channel = squeeze(gain_matrix(iPair,:)); 
-        gain_channel(gain_channel <  10^(threshold_value)*max_gain) = 0;
-
-
-        for iCluster = 1:length(iRois)
-            sROI = sCortex.Atlas(iAtlas).Scouts(iRois(iCluster));
-            vertex = sROI.Vertices;
-
-            T{iRow, sROI.Label} = sum(gain_channel(vertex));
-        end
-
-        T{iRow,'Channel'} = sForward.pair_names(iPair);
-        iRow = iRow + 1;
-    end
     
     
     fileName = sProcess.options.outputdir.Value{1};
     writetable(T,fileName, 'FileType','text');
-
-
 end
