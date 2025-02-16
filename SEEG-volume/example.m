a4a300f63af43d0562cb06dcfa6a3d820c9c0229
Edouard2laire <edouard.delaire@icloud.com>
2025-02-16 17:58:12 -0500

    Add process to convert SEEG electrodes to a 3D mask (#6)
    * Add files
    
    * avoid duplicated name in the database
    
    * change default radius to 10 mm

---
+++ SEEG-volume/example.m
@@ -0,0 +1,12 @@
+
+% Input definition
+subject_name = 'PA65-P';
+channel_file = 'PA65-P/PA65S2/channel.mat';
+contact_name = {'RH4', 'RH5', 'RH8'};
+radius       = 10;
+
+% Output definition
+output_name = 'SEEG_VOI';
+
+[VOI_file,output_vol] = SEEG_to_mask(subject_name, channel_file, contact_name, radius, output_name);
+
